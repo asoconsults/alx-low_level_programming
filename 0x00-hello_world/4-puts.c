f2d1427187dbb777e9e88547887018b6f355bc3e
asoconsults <asoconsult110@gmail.com>
2022-10-13 04:05:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - Coding
+ *
+ *Return: Always 0 (correct)
+ *
+ */
+
+int main (void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
