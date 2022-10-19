f4409ca612667482a0785a704acb4363394718b6
asoconsults <asoconsult110@gmail.com>
2022-10-19 02:40:14 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10.c - print the alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ton;
+	char le;
+
+	for (ton = 0; ton <= 9; ton++)
+	{
+		for (le = 'a'; le <= 'z'; le++)
+			_putchar(le);
+		_putchar('\n');
+	}
+}
