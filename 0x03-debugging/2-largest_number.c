c56a79fd0b763f4d9cea17897041099b6426e313
asoconsults <asoconsult110@gmail.com>
2022-10-20 05:09:07 -0700

    2-largest_number.c

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * largest_number - returns the largest number
+ * @a: first value
+ * @b: second value
+ * @c: third value
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (s > v)
+	{
+		if (v > z)
+			largest = s;
+		else if (s > z)
+			largest = s;
+		else
+			largest = z;
+	}
+	else
+	{
+		if (v < z)
+			largest = z;
+		else
+			largest = v;
+	}
+	return (largest);
+}
