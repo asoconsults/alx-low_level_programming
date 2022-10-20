0275d0d29dc3838ea8998ba7774f430a8f49a41e
asoconsults <asoconsult110@gmail.com>
2022-10-20 14:44:11 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,15 +3,15 @@
 /**
  * print_line - prints a straight
  * @n: the number to be printed
+ * Return: void
  */
 void print_line(int n)
 {
 	int len;
 
-	if (n > 0)
+	for (len = 0; len < n; len++)
 	{
-		for (len = 0; len < n; len++)
-			_putchar('_');
+		_putchar('_');
 	}
 	_putchar('\n')
 }
