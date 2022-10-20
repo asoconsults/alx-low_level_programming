beded0c65f94d43a472ad89100616856a32a3a9c
asoconsults <asoconsult110@gmail.com>
2022-10-20 06:41:10 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,7 +27,7 @@ void print_remaining_days(int month, int day, int year)
 		else
 		{
 			printf("Day of the yer: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
