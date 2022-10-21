1e7be08fbe5cd5d88b186c098e80698a93909ff8
asoconsults <asoconsult110@gmail.com>
2022-10-20 18:17:50 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,6 @@
 /**
  * print_triangle - prints a triangle
  * @n: size of triangle
- * Return: Always 0
  */
 void print_triangle(int n)
 {
@@ -16,7 +15,7 @@ void print_triangle(int n)
 				_putchar(' ');
 
 			for (tri = 0; tri < h; tri++)
-				_putchar('0');
+				_putchar('#');
 
 			if (h == n)
 				continue;
